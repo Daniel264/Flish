7cac3b478ab2cf4a3532afac467eee6b06bf5a8c
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-07-29 13:53:11 +0100

    Round up the second MidDiv

--- src/components/MidDiv.tsx
+++ src/components/MidDiv.tsx
@@ -39,19 +39,25 @@ const MidDiv = () => {
   return (
     <>
         <div className="flex flex-row px-14">
-          <div className='w-2/4 mt-28'><h1 className="text-5xl font-semibold font-serif">Flish</h1>
+          <div className='w-fit pr-8 mt-28'><h1 className="text-5xl font-semibold font-serif">Flish</h1>
           <p className='text-2xl'>The Internet's source for visuals.</p>
           <p className='text-2xl'>Powered by creators everywhere</p>
           <input
                 placeholder="search for images and illustrations"
-                className="bg-gray-200 py-3 px-48 rounded-lg "
+                className="bg-gray-200 py-3 px-48 rounded-lg mt-16"
                 type="text"
               />
           </div>
-          <div className='mt-14'>
-            {photo.length > 0 && <img className='h-64 w-72 rounded-lg' src={photo[0].urls.regular} />}
+          <div className='mt-14 relative'>
+            {photo.length > 0 && <img className='h-72 w-80 rounded-lg' src={photo[0].urls.regular} />}
+            <div className='text-white absolute bottom-0 p-8 text-sm font-bold'>Discover Flish++<br />
+            Unlimited Pictures<br />
+            No Ads</div>
+
           </div>
-          <div></div>
+          <div className=''>
+              
+          </div>
         </div>
     </>
   )
